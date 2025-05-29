476ea7ef8c5e2ca8d82c9c713ce802aef0eff3cb
RapidFuge <rapidfugegt1@gmail.com>
2025-05-30 04:56:03 +0800

    Aded clearer redirect indiator.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 // pages/_app.tsx
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import { ProgressProvider } from "@bprogress/next/pages";
 import "../styles/globals.css";
 import "prismjs/themes/prism-okaidia.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ProgressProvider
+      height="3px"
+      color="#2563eb"
+      options={{ showSpinner: false }}
+      shallowRouting
+    >
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ProgressProvider>
   );
 }
 
